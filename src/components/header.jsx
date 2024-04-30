4a653af782d9c4e87edc8c40af933611cf7a7865
Steve Evrard <stevelevrard@gmail.com>
2024-04-30 11:58:45 -0400

    add app store button and initial seo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,6 +12,11 @@ export const Header = (props) => {
                   <img src="img/hunched-logo-white.png" className="img-responsive" alt="" />
                 </div>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                  <a href="https://apps.apple.com/us/app/hunched-text-neck-solution/id6450959041" target="_blank" rel="noopener noreferrer">
+                    <img src="img/app-store-badge.svg" className="img-responsive app-store-badge" alt="" />
+                  </a>
+                </div>
                 <a
                   href="#about"
                   className="btn btn-custom btn-lg page-scroll"
